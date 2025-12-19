aeeec82ab62678877de8c8d4807cd69d57024a47
William McIlleron <williamjonahmci@icloud.com>
2025-12-19 17:32:53 +0200

    Finish all styling +hamburger

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function scrollToMenu() {
       header.classList.remove('scrolled');
     }
   });
-  
+  //Gallery Opener
     document.querySelector('.see-more').addEventListener('click', () => {
         window.open('./gallery.html', '_self')
     }) 
@@ -37,3 +37,17 @@ showLoader();
   window.addEventListener('load', () => {
     hideLoader();
   })
+
+  // Hamburger menu
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  hamburger.addEventListener("click", ()  => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  })
+
+  document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }))
